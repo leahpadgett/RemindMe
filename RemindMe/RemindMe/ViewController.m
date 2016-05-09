71ab75ee4cd8566406554085285d89e21920d89b
Leah Padgett <lmp0024@yahoo.com>
2016-05-09 11:12:23 -0400

    Home, Add Reminder, 'Upcoming Reminders' screens added.

--- RemindMe/RemindMe/ViewController.m
+++ RemindMe/RemindMe/ViewController.m
@@ -10,13 +10,25 @@
 
 @interface ViewController ()
 
+
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//[self.navigationController.navigationBar setTranslucent:NO];
+    
+    
+    // Logo as top nav bar title
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"RemindMelogo.png"]];
+    
+
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
